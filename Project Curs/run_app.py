62863a93478eeb266bca9ef724c44fd9cb3d62b1
krusag <74322756+krusag@users.noreply.github.com>
2025-05-06 20:50:57 +0300

    Add files via upload
    Обновлённый код, изменена структура и улучшен функционал

--- Project Curs/run_app.py
+++ Project Curs/run_app.py
@@ -7,21 +7,15 @@ import threading
 from app import create_app
 import os
 
+# Определяем пути к шаблонам и статическим файлам
 template_dir = os.path.join(os.path.dirname(__file__), "app", "templates")
 static_dir = os.path.join(os.path.dirname(__file__), "static")
-
-app = create_app(template_folder=template_dir, static_folder=static_dir)
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 
 def open_browser():
     webbrowser.open("http://127.0.0.1:5000")
 
 # Создаём экземпляр приложения Flask
-app = create_app()
+app = create_app(template_folder=template_dir, static_folder=static_dir)
 
 if __name__ == "__main__":
     # Открываем браузер через 1 секунду после запуска сервера
